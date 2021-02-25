6d6475068c86982c143911a22e88ccd5c86be8ca
Miikka Kärkkäinen <miikkak@tuta.io>
2021-02-25 11:56:23 -0600

    basic styling

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,8 +1,19 @@
 import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import alertSound from "./test-sound.mp3";
+import { Typography, Container, Grid, Button } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+}));
 
 export default function Timer() {
-  const [minutes, setMinutes] = useState(1);
+  const classes = useStyles();
+
+  const [minutes, setMinutes] = useState(4);
   const [seconds, setSeconds] = useState(0);
   const [displayMessage, setDisplayMessage] = useState(false);
   const [start, setStart] = useState(false);
@@ -43,16 +54,57 @@ export default function Timer() {
     setDisplayMessage(false);
   };
 
+  const handleReset = () => {
+    setStart(false);
+    setDisplayMessage(false);
+    setMinutes(4);
+    setSeconds(0);
+  };
+
   const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
   const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
 
   return (
-    <div>
-      <div>{displayMessage && <div>Scoop the top layer of coffee!</div>}</div>
-      <button onClick={() => handleStart()}>{start ? "Stop" : "Start"}</button>
-      <div>
-        {timerMinutes}:{timerSeconds}
-      </div>
+    <div className={classes.heroContent}>
+      <Container maxWidth="sm">
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="textPrimary"
+          gutterBottom
+        >
+          {timerMinutes}:{timerSeconds}
+        </Typography>
+
+        <Grid container spacig={2} justify="center">
+          <Grid item>
+            <div>
+              {displayMessage && (
+                <Typography>Scoop the top layer of coffee!</Typography>
+              )}
+            </div>
+          </Grid>
+          <Grid item>
+            <Button
+              onClick={() => handleStart()}
+              variant="contained"
+              color="primary"
+            >
+              {start ? "Stop" : "Start"}
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button
+              onClick={() => handleReset()}
+              variant="outlined"
+              color="secondary"
+            >
+              Reset
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
